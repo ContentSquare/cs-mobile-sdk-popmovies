34a044e48cc2dcdb57aed159d451215a5532e72b
Kailin <kailin@MacBook-Pro-de-ContentSquare.local>
2017-04-21 14:58:08 +0200

    Adding Fabric to demo app

--- app/src/main/java/com/dmitrymalkovich/android/popularmoviesapp/MainApp.java
+++ app/src/main/java/com/dmitrymalkovich/android/popularmoviesapp/MainApp.java
@@ -3,11 +3,14 @@ package com.dmitrymalkovich.android.popularmoviesapp;
 import android.app.Application;
 
 import com.contentsquare.android.ContentSquare;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         ContentSquare.startWithProjectId(getApplicationContext(), "popularmoviesapp");
     }
 }
